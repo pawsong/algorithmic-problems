ae0111a97274d00ee963f0f68c63b609753d5f2a
Dokyung Song <dokyung.song@gmail.com>
2015-12-28 22:46:30 +0900

    Fix typo.

--- problems/knapsack/solution.cc
+++ problems/knapsack/solution.cc
@@ -5,14 +5,14 @@ int Solution::knapsackWithRepetition(const std::vector<KnapsackItem>& items, int
 
   std::vector<int> maxValues (maxWeight + 1);
 
-  // The max value for the zero-weight capacity back is zero.
+  // The max value for the zero-weight capacity knapsack is zero.
   maxValues[0] = 0;
 
   // Solve the rest of subproblems.
   for ( int j=1; j<=maxWeight; j++ ) {
     int maxValue = 0;
 
-    // Look up solutions of interest.
+    // Each subproblem takes up to O(n) to compute.
     for ( int i=0; i<items.size(); i++ ) {
       if ( j < items[i].weight )
         continue;

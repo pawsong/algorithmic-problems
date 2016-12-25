3b834eaea7bcef5f2cd64317911e9496a5b4bf12
Dokyung Song <dokyung.song@gmail.com>
2016-12-25 13:29:09 -0800

    Refine solution to the Knapsack problem

--- problems/knapsack/solution.cc
+++ problems/knapsack/solution.cc
@@ -1,29 +1,17 @@
-
 #include "solution.h"
 
 int Solution::knapsackWithRepetition(const std::vector<KnapsackItem>& items, int maxWeight) {
-
-  std::vector<int> maxValues (maxWeight + 1);
-
-  // The max value for the zero-weight capacity knapsack is zero.
-  maxValues[0] = 0;
+  std::vector<int> maxValues( maxWeight + 1, 0 );
 
   // Solve the rest of subproblems.
-  for ( int j=1; j<=maxWeight; j++ ) {
-    int maxValue = 0;
-
-    // Each subproblem takes up to O(n) to compute.
-    for ( int i=0; i<items.size(); i++ ) {
-      if ( j < items[i].weight )
-        continue;
-
+  for ( int i=0; i<items.size(); i++ ) {
+    for ( int j=items[i].weight; j<=maxWeight; j++ ) {
       int value = maxValues[ j - items[i].weight ] + items[i].value;
-      if ( value > maxValue ) {
-        maxValue = value;
+      if ( value > maxValues[j] ) {
+        maxValues[j] = value;
       }
     }
-    maxValues[j] = maxValue;
   }
 
-  return maxValues[maxValues.size() - 1];
+  return maxValues.back();
 }
--- problems/knapsack/test.cc
+++ problems/knapsack/test.cc
@@ -1,4 +1,3 @@
-
 #include <vector>
 
 #include "gmock/gmock.h"
@@ -16,8 +15,7 @@ KnapsackItem createKnapsackItem(int weight, int value) {
   return item;
 }
 
-TEST(KnapsackWithRepetitionTest, SmallCase) {
-
+TEST(KnapsackWithRepetitionTest, SmallCase1) {
   std::vector<KnapsackItem> items;
   items.push_back(createKnapsackItem(6, 30));
   items.push_back(createKnapsackItem(3, 14));
@@ -29,4 +27,21 @@ TEST(KnapsackWithRepetitionTest, SmallCase) {
   int expected = 30 + 9 + 9;
 
   EXPECT_EQ(actual, expected);
+}
+
+TEST(KnapsackWithRepetitionTest, SmallCase2) {
+  std::vector<KnapsackItem> items;
+  items.push_back(createKnapsackItem(6, 6));
+  items.push_back(createKnapsackItem(3, 14));
+  items.push_back(createKnapsackItem(4, 16));
+  items.push_back(createKnapsackItem(2, 9));
+
+  Solution sol;
+  int actual = sol.knapsackWithRepetition(items, 10);
+  int expected = 2 * 14 + 2 * 9;
+
+  EXPECT_EQ(actual, expected);
+
+  items.push_back(createKnapsackItem(10, 100));
+  EXPECT_EQ(100, sol.knapsackWithRepetition(items, 10));
 }
